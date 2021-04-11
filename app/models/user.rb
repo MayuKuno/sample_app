acec26e3fd310167972a386f707cd975923a33b5
mayukuno <kunomayu1006@gmail.com>
2021-04-11 18:33:23 +0900

    chap13.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
+
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -67,6 +69,12 @@ class User < ApplicationRecord
   def password_reset_expired?
     self.reset_sent_at < 2.hours.ago
   end
+
+  # 試作feedの定義
+  # 完全な実装は次章の「ユーザーをフォローする」を参照
+  def feed
+    Micropost.where("user_id = ?", self.id)
+  end
   private
 
   # メールアドレスをすべて小文字にする
@@ -79,4 +87,6 @@ class User < ApplicationRecord
     self.activation_token  = User.new_token
     self.activation_digest = User.digest(activation_token)
   end
+
+
 end
